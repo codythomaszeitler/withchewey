bc27ce3dcde74a48baf1f34ef7dbbf5bee61ef74
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-03-31 01:14:05 -0700

    fix:  garbo solution

--- src/main/java/com/zsi/TrappingRainWater.java
+++ src/main/java/com/zsi/TrappingRainWater.java
@@ -5,17 +5,67 @@ import java.util.List;
 
 public class TrappingRainWater {
 
+    public int trap(int[] height) {
+        List<Convex> initial = getConvexes(height);
+
+        int[] peaks = new int[initial.size()];
+        for (int i = 0; i < initial.size(); i++) {
+            int index = initial.get(i).index;
+            peaks[i] = height[index];
+        }
+
+        List<Convex> completed = getConvexes(peaks);
+
+        List<Convex> toComputeWith = new ArrayList<>();
+        if (!completed.contains(initial.get(0))) {
+            toComputeWith.add(initial.get(0));
+        }
+
+        for (Convex a : completed) {
+            Convex b = initial.get(a.index);
+            toComputeWith.add(b);
+        }
+
+        if (!completed.contains(initial.get(initial.size() - 1))) {
+            toComputeWith.add(initial.get(initial.size() - 1));
+        }
+
+        int capturedWater = 0;
+
+        for (int i = 0; i < toComputeWith.size() - 1; i++) {
+            Convex left = toComputeWith.get(i);
+            Convex right = toComputeWith.get(i + 1);
+
+            capturedWater += calculate(left, right, height);
+        }
+
+        return capturedWater;
+    }
+
+    public static int calculate(Convex left, Convex right, int[] heights) {
+        int lower = Math.min(heights[left.index], heights[right.index]);
+        int box = (right.index - left.index - 1) * lower;
+
+        for (int i = left.index + 1; i < right.index; i++) {
+            box -= heights[i];
+        }
+
+        return box;
+    }
+
     public static List<Convex> getConvexes(int[] heights) {
         ArrayList<Convex> convexes = new ArrayList<>();
 
-        // Convex convex = findNextConvex(heights, 0);
-        // convexes.add(convex);
-        // do {
-        // convex = findNextConvex(heights, convex.index + 1);
-        // if (convex != null) {
-        // convexes.add(convex);
-        // }
-        // } while (convex != null);
+        if (heights.length == 1) {
+            convexes.add(new Convex(0));
+            return convexes;
+        }
+
+        if (heights.length == 2) {
+            convexes.add(new Convex(0));
+            convexes.add(new Convex(1));
+            return convexes;
+        }
 
         Convex maybeConvex = null;
         for (int i = 0; i < heights.length; i++) {
@@ -35,42 +85,23 @@ public class TrappingRainWater {
                 int left = heights[i - 1];
                 int right = heights[i + 1];
 
+                if (left < middle) {
+                    maybeConvex = new Convex(i);
+                }
+
                 if (left < middle && middle > right) {
                     convexes.add(new Convex(i));
-                } else if (left < middle && middle == right && maybeConvex == null) {
-                    maybeConvex = new Convex(i);
-                } else if (left < middle && middle == right && maybeConvex != null) {
-                    maybeConvex = new Convex(i);
-                } else if (left == middle && middle > right && maybeConvex != null) {
-                    convexes.add(maybeConvex);
-                    convexes.add(new Convex(i));
-                } else if (left == middle && middle > right && maybeConvex == null) {
-                    convexes.add(new Convex(i));
+                    maybeConvex = null;
+                } else if (left == middle && middle > right) {
+                    if (maybeConvex != null) {
+                        convexes.add(maybeConvex);
+                        convexes.add(new Convex(i));
+                    }
                 }
             }
         }
 
         return convexes;
-    }
-
-    // So this really is not correct.
-    public static Convex findNextConvex(int[] heights, int previousIndex) {
-
-        for (int i = previousIndex + 1; i < heights.length; i++) {
-            if (heights[previousIndex] <= heights[i]) {
-                break;
-            }
-            previousIndex = i;
-        }
-
-        for (int i = previousIndex + 1; i < heights.length; i++) {
-            if (heights[previousIndex] > heights[i]) {
-                return new Convex(previousIndex);
-            }
-            previousIndex = i;
-        }
-
-        return null;
     }
 
     public static class Convex {
--- src/test/java/com/zsi/TrappingRainWaterTest.java
+++ src/test/java/com/zsi/TrappingRainWaterTest.java
@@ -3,6 +3,7 @@ package com.zsi;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.junit.Test;
 
@@ -22,18 +23,113 @@ public class TrappingRainWaterTest {
     }
 
     @Test
-    public void testFindFirstNonIncreasingNumberIndex() {
-        int[] heights = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-
-        TrappingRainWater.Convex convex = TrappingRainWater.findNextConvex(heights, 0);
-        assertEquals(convex, new TrappingRainWater.Convex(1));
-    }
-
-    @Test
     public void testFindConvexesOnHill() {
         int[] heights = new int[] { 3, 5, 5, 5, 5, 5, 2 };
         List<TrappingRainWater.Convex> convexes = TrappingRainWater.getConvexes(heights);
 
         assertEquals(2, convexes.size());
+    }
+
+    @Test
+    public void testFindConvexesWithSingularHillPoint() {
+        int[] heights = new int[] { 0, 2, 2, 10, 2, 2, 0 };
+        List<TrappingRainWater.Convex> convexes = TrappingRainWater.getConvexes(heights);
+
+        assertEquals(1, convexes.size());
+
+        assertEquals(convexes.get(0), new TrappingRainWater.Convex(3));
+    }
+
+    @Test
+    public void testSmallSpeedBumps() {
+        int[] heights = new int[] { 1, 0, 1, 0, 1, 0, 1 };
+        List<TrappingRainWater.Convex> convexes = TrappingRainWater.getConvexes(heights);
+
+        assertEquals(4, convexes.size());
+    }
+
+    @Test
+    public void testSmallSpeedBumpsNoEnding() {
+        int[] heights = new int[] { 1, 0, 1, 0, 1, 0 };
+        List<TrappingRainWater.Convex> convexes = TrappingRainWater.getConvexes(heights);
+
+        assertEquals(3, convexes.size());
+    }
+
+    @Test
+    public void testSmallSpeedBumpsBigGap() {
+        int[] heights = new int[] { 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1 };
+        List<TrappingRainWater.Convex> convexes = TrappingRainWater.getConvexes(heights);
+
+        assertEquals(4, convexes.size());
+    }
+
+    @Test
+    public void testSuperSmallConvexCall() {
+        int[] heights = new int[] { 5, 5 };
+        List<TrappingRainWater.Convex> convexes = TrappingRainWater.getConvexes(heights);
+
+        assertEquals(2, convexes.size());
+    }
+
+    @Test
+    public void testSingularSquareBucket() {
+        int[] heights = new int[] { 5, 0, 0, 0, 5 };
+
+        TrappingRainWater solution = new TrappingRainWater();
+        int actual = solution.trap(heights);
+
+        assertEquals(15, actual);
+    }
+
+    @Test
+    public void testSimpleCalculate() {
+        int[] heights = new int[] { 5, 0, 0, 0, 5 };
+
+        int actual = TrappingRainWater.calculate(new TrappingRainWater.Convex(0), new TrappingRainWater.Convex(4),
+                heights);
+
+        assertEquals(15, actual);
+    }
+
+    @Test
+    public void testSimpleCalculateWithBumpyUnderneath() {
+        int[] heights = new int[] { 5, 2, 0, 3, 5 };
+
+        int actual = TrappingRainWater.calculate(new TrappingRainWater.Convex(0), new TrappingRainWater.Convex(4),
+                heights);
+
+        assertEquals(10, actual);
+    }
+
+    @Test
+    public void testSimpleCalculateAllFilled() {
+        int[] heights = new int[] { 5, 5, 5, 5, 5 };
+
+        int actual = TrappingRainWater.calculate(new TrappingRainWater.Convex(0), new TrappingRainWater.Convex(4),
+                heights);
+
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void testMoreComplexExample() {
+
+        int[] heights = new int[] { 4, 2, 0, 3, 2, 5 };
+
+        TrappingRainWater solution = new TrappingRainWater();
+        int actual = solution.trap(heights);
+
+        assertEquals(9, actual);
+    }
+
+    @Test
+    public void test1() {
+        int[] heights = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+        TrappingRainWater solution = new TrappingRainWater();
+        int actual = solution.trap(heights);
+
+        assertEquals(6, actual);
     }
 }
